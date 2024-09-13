6194130936a492ad22e0f1c98a94c8465c47502a
Betiza Barreira <be.penques@hotmail.com>
2024-09-13 16:02:34 -0300

    funcionalidades - summary fetch http

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,33 @@
 import { Dialog } from './components/ui/dialog'
-import { CreateGoal } from './components/create-goal'
+
 import { Summary } from './components/summary'
-// import { EmptyGoals } from './components/empty-goals'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  // const [summary, setSummary] = useState<SummaryType | null>(null)
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/summary')
+  //     .then(response => {
+  //       return response.json()
+  //     })
+  //     .then(data => {
+  //       setSummary(data.summary)
+  //     })
+  // }, [])
+
+  //https://tanstack.com/query/latest/docs/framework/react/quick-start
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 segundos - qto tempo em milessegundos quero considerar que esse dado não é obsoleto
+  })
+
   return (
     <Dialog>
-      <Summary />
-      {/* <EmptyGoals /> */}
-      <CreateGoal />
+      {data?.total && data?.total > 0 ? <Summary /> : <EmptyGoals />}
     </Dialog>
   )
 }
---
+++ src/components/pending-goal.tsx
@@ -0,0 +1,42 @@
+import { Plus } from 'lucide-react'
+import { OutlineButton } from './ui/outline-button'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { createGoalCompletion } from '../http/create-goal-completion'
+
+export function PendingGoals() {
+  const queryClient = useQueryClient()
+
+  const { data } = useQuery({
+    queryKey: ['pendingGoals'],
+    queryFn: getPendingGoals,
+    staleTime: 1000 * 60, // 60 segundos - qto tempo em milessegundos quero considerar que esse dado não é obsoleto
+  })
+  if (!data) {
+    return null
+  }
+
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] }) //atualizar o carregamento de summary
+    queryClient.invalidateQueries({ queryKey: ['pendingGoals'] })
+  }
+
+  return (
+    <div className="flex flex-wrap  gap-3">
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
